cfd0561dda424ad79a9fca35146812f20be71ede
Austin Colt <austin@jcolt.net>
2024-10-29 22:14:40 -0600

    Add a styled header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import ServerStatus from './components/ServerStatus';
 export default function Home() {
   return (
     <main className="home-container">
-        <h1 className="text-4xl">Homelab Dashboard Test</h1>
-        <ServerStatus />
+        <div className="w-3/12 h-32 bg-blue-900 mx-auto mt-4 content-center rounded-3xl">
+            <h1 className="text-4xl text-center">Homelab Dashboard</h1>
+        </div>
+        <ServerStatus/>
     </main>
   );
 }
